5da90f55e9b25671d4fea562c71f345e4a3c3816
grattatata <extrobin2121@gmail.com>
2022-09-20 12:12:57 +0900

    list first commit

--- app.py
+++ app.py
@@ -5,24 +5,51 @@ app = Flask(__name__)
 import requests
 from bs4 import BeautifulSoup
 
-# 일단 각자 파이몽고 사용해보기! 본인 디비 주소로 바꾸세요!
 from pymongo import MongoClient
-client = MongoClient('mongodb+srv://password:sparta@cluster0.kythmsg.mongodb.net/?retryWrites=true&w=majority')
-db = client.worth_seeing
+import certifi
+client = MongoClient('mongodb+srv://test:sparta@cluster0.znplsth.mongodb.net/?retryWrites=true&w=majority', tlsCAFile=certifi.where())
+db = client.dbsparta
 
 
 # HTML 화면 보여주기
 @app.route('/')
 def main():
-    return render_template('main.html')
+    return render_template('list.html')
 
 @app.route('/signup')
 def signup():
     return render_template('signup.html')
 
-@app.route('/list')
-def list():
-    return render_template('list.html')
+@app.route("/list", methods=["POST"])
+def game_post():
+    url_receive = request.form['url_give']
+    star_receive = request.form['star_give']
+    comment_receive = request.form['comment_give']
+
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+    data = requests.get(url_receive, headers=headers)
+
+    soup = BeautifulSoup(data.text, 'html.parser')
+    title = soup.select_one('meta[property="og:title"]')['content']
+    desc = soup.select_one('meta[property="og:description"]')['content']
+    image = soup.select_one('meta[property="og:image"]')['content']
+
+    doc = {
+        'title':title,
+        'image':image,
+        'desc':desc,
+        'star':star_receive,
+        'comment':comment_receive
+    }
+    db.youtube.insert_one(doc)
+
+    return jsonify({'msg':'게임 추천 완료!'})
+
+@app.route("/list", methods=["GET"])
+def youtube_get():
+    youtube_list = list(db.youtube.find({}, {'_id':False}))
+    return jsonify({'youtube_list': youtube_list})
 
 @app.route('/comment')
 def comment():

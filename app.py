9d83b88a4067e3100b2d1ec929bdeb1452ff950c
doeun <112849712+dodoeun@users.noreply.github.com>
2022-09-22 17:39:17 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -1,103 +1,40 @@
-#flask, pymongo, dnspython,requests, bs4 일단 이렇게만 했습니다.
-from flask import Flask, render_template, jsonify, request
+import jwt
+import datetime
+import hashlib
+from flask import Flask, render_template, jsonify, request, redirect, url_for
+from werkzeug.utils import secure_filename
+from datetime import datetime, timedelta
+
+import requests
+from bs4 import BeautifulSoup
+
+from pymongo import MongoClient
+client = MongoClient('mongodb+srv://test:sparta@cluster0.kythmsg.mongodb.net/?retryWrites=true&w=majority')
+db = client.dbsparta
+
 app = Flask(__name__)
-SECRET_KEY = 'SPARTA'
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 app.config['UPLOAD_FOLDER'] = "../static/profile_pics"
 
-import requests, hashlib, jwt
-from bs4 import BeautifulSoup
+SECRET_KEY = 'SPARTA'
 
-from datetime import datetime, timedelta
-from pymongo import MongoClient
-import certifi
-client = MongoClient('mongodb+srv://test:sparta@cluster0.znplsth.mongodb.net/?retryWrites=true&w=majority', tlsCAFile=certifi.where())
-db = client.dbsparta
-
-
-# HTML 화면 보여주기
-@app.route('/')
-def main():
-    return render_template('list.html')
-
-@app.route('/signup')
-def signup():
-    return render_template('signup.html')
-
-@app.route("/list", methods=["POST"])
-def channel_post():
-    url_receive = request.form['url_give']
-    comment_receive = request.form['comment_give']
-    video_receive = request.form['video_give']
-
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
-    data = requests.get(url_receive, headers=headers)
-
-    soup = BeautifulSoup(data.text, 'html.parser')
-    title = soup.select_one('meta[property="og:title"]')['content']
-    image = soup.select_one('meta[property="og:image"]')['content']
-
-    doc = {
-        'title':title,
-        'image':image,
-        'comment':comment_receive,
-        'video': video_receive
-    }
-    db.list.insert_one(doc)
-
-    return jsonify({'msg':'채널 추천 완료!'})
-
-@app.route("/list", methods=["GET"])
-def youtube_get():
-    youtube_list = list(db.list.find({}, {'_id':False}))
-    return jsonify({'youtube_list': youtube_list})
-
-@app.route('/comment')
-def comment():
-    return render_template('comment.html')
-
-# 진자2로 db에서 프로필 값 보내주기인데 확실하지 않아요..안되면 프로필 빼고 가는걸로..
-# @app.route('/comment')
-# def comment():
-#     profile = list(db.worthseeing.find({}, {"_id": False}))
-#     return render_template('index.html', profile=profile)
-
-# 코멘트 db 저장
-@app.route("/comment", methods=["POST"])
-def comment_post():
-    comment_receive = request.form['comment_give']
-
-    doc = {
-        'comment': comment_receive
-    }
-    db.worthseeing.insert_one(doc)
-
-    return jsonify({'msg': '등록 완료'})
-
-# 코멘트 html 보여주기
-@app.route("/comment", methods=["GET"])
-def comment_get():
-    all_comments = list(db.worthseeing.find({}, {'_id': False}))
-    return jsonify({'comments': all_comments})
-
+#로그인 하면 카드 리스트로 이동하기
 @app.route('/')
 def home():
     token_receive = request.cookies.get('mytoken')
     try:
         payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
-        return render_template('main.html')
+        user_info = db.users.find_one({"username": payload["id"]})
+        return render_template('list.html', user_info=user_info)
     except jwt.ExpiredSignatureError:
         return redirect(url_for("login", msg="로그인 시간이 만료되었습니다."))
     except jwt.exceptions.DecodeError:
         return redirect(url_for("login", msg="로그인 정보가 존재하지 않습니다."))
 
-
 @app.route('/login')
 def login():
     msg = request.args.get("msg")
     return render_template('login.html', msg=msg)
-
 
 @app.route('/sign_in', methods=["POST"])
 def sign_in():
@@ -126,37 +63,162 @@ def sign_in():
     else:
         return jsonify({'result': 'fail', 'msg': '아이디/비밀번호가 일치하지 않습니다.'})
 
-
 # 회원가입 페이지
 @app.route('/sign_up')
 def sign_up_page():
     return render_template('sign_up.html')
-
 
 @app.route('/sign_up/save', methods=['POST'])
 def sign_up():
     username_receive = request.form['username_give']
     password_receive = request.form['password_give']
     password_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
-
     doc = {
-        "username": username_receive,  # 아이디
-        "password": password_hash,  # 비밀번호
-        "profile_name": username_receive,  # 프로필 이름 기본값은 아이디
-        # "profile_pic": "",                                          # 프로필 사진 파일 이름
-        # "profile_pic_real": "profile_pics/profile_placeholder.png", # 프로필 사진 기본 이미지
+        "username": username_receive,
+        "password": password_hash,
+        "profile_name": username_receive,
+        "profile_pic": "",
+        "profile_pic_real": "profile_pics/profile_placeholder.png",
+        "profile_info": ""
     }
-
     db.users.insert_one(doc)
     return jsonify({'result': 'success'})
-
 
 # 아이디 중복 검사
 @app.route('/sign_up/check_dup', methods=['POST'])
 def check_dup():
     username_receive = request.form['username_give']
     exists = bool(db.users.find_one({"username": username_receive}))
+    # print(value_receive, type_receive, exists)
     return jsonify({'result': 'success', 'exists': exists})
+
+#로그인한 사람의 정보 보기
+@app.route('/mypage/<username>')
+def mypage(username):
+    # 각 사용자의 프로필과 글을 모아볼 수 있는 공간
+    token_receive = request.cookies.get('mytoken')
+    try:
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+        status = (username == payload["id"])  # 내 프로필이면 True, 다른 사람 프로필 페이지면 False
+
+        user_info = db.users.find_one({"username": username}, {"_id": False})
+        return render_template('mypage.html', user_info=user_info, status=status)
+    except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
+        return redirect(url_for("home"))
+
+#내 정보 수정하기
+@app.route('/update_profile', methods=['POST'])
+def save_img():
+    token_receive = request.cookies.get('mytoken')
+    try:
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+        username = payload["id"]
+        name_receive = request.form["name_give"]
+        about_receive = request.form["about_give"]
+        new_doc = {
+            "profile_name": name_receive,
+            "profile_info": about_receive
+        }
+        if 'file_give' in request.files:
+            file = request.files["file_give"]
+            filename = secure_filename(file.filename)
+            extension = filename.split(".")[-1]
+            file_path = f"profile_pics/{username}.{extension}"
+            file.save("./static/"+file_path)
+            new_doc["profile_pic"] = filename
+            new_doc["profile_pic_real"] = file_path
+        db.users.update_one({'username': payload['id']}, {'$set':new_doc})
+        return jsonify({"result": "success", 'msg': '프로필을 업데이트했습니다.'})
+    except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
+        return redirect(url_for("home"))
+
+#리스트에서 카드 정보 디비에 저장하기
+@app.route("/list", methods=["POST"])
+def channel_post():
+    url_receive = request.form['url_give']
+    comment_receive = request.form['comment_give']
+    video_receive = request.form['video_give']
+    video1_receive = request.form['video1_give']
+
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+    data = requests.get(url_receive, headers=headers)
+
+    soup = BeautifulSoup(data.text, 'html.parser')
+    title = soup.select_one('meta[property="og:title"]')['content']
+    image = soup.select_one('meta[property="og:image"]')['content']
+
+    doc = {
+        'title':title,
+        'image':image,
+        'comment':comment_receive,
+        'video': video_receive,
+        'video1': video1_receive
+    }
+    db.list.insert_one(doc)
+
+    return jsonify({'msg':'채널 추천 완료!'})
+
+#카드 정보 조회하기
+@app.route("/list", methods=["GET"])
+def youtube_get():
+    youtube_list = list(db.list.find({}, {'_id':False}))
+    return jsonify({'youtube_list': youtube_list})
+
+#로그인 되면 랜덤 채널 보여주기
+@app.route('/comment')
+def comment():
+    token_receive = request.cookies.get('mytoken')
+    try:
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+        user_info = db.users.find_one({"username": payload["id"]})
+        return render_template('comment.html', user_info=user_info)
+    except jwt.ExpiredSignatureError:
+        return redirect(url_for("login", msg="로그인 시간이 만료되었습니다."))
+    except jwt.exceptions.DecodeError:
+        return redirect(url_for("login", msg="로그인 정보가 존재하지 않습니다."))
+
+#랜덤 채널을 보여주기 위해 디비에서 꺼내오기
+@app.route('/comment/get', methods=["GET"])
+def random_get():
+    random_list = list(db.list.find({}, {'_id':False}))
+    return jsonify({'random_list': random_list})
+
+#코멘트 포스팅하기
+@app.route('/posting', methods=['POST'])
+def posting():
+    token_receive = request.cookies.get('mytoken')
+    try:
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+        user_info = db.users.find_one({"username": payload["id"]})
+        comment_receive = request.form["comment_give"]
+        date_receive = request.form["date_give"]
+        print(type(date_receive))
+        doc = {
+            "username": user_info["username"],
+            "profile_name": user_info["profile_name"],
+            "profile_pic_real": user_info["profile_pic_real"],
+            "comment": comment_receive,
+            "date": date_receive
+        }
+        db.posts.insert_one(doc)
+        return jsonify({"result": "success", 'msg': '포스팅 성공'})
+    except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
+        return redirect(url_for("home"))
+
+#포스팅한 코멘트 가져오기
+@app.route("/get_posts", methods=['GET'])
+def get_posts():
+    token_receive = request.cookies.get('mytoken')
+    try:
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+        posts = list(db.posts.find({}).sort("date", -1).limit(20))
+        for post in posts:
+            post["_id"] = str(post["_id"])
+        return jsonify({"result": "success", 'msg': '포스팅을 가져왔습니다.', "posts": posts})
+    except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
+        return redirect(url_for("home"))
+
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)

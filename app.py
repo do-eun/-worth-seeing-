e012cf61fb1687bed0bbccac4fe12c2727e1753b
jonghyeong1 <91271519+jonghyeong1@users.noreply.github.com>
2022-09-22 16:54:16 +0900

    Update app.py

--- app.py
+++ app.py
@@ -77,5 +77,82 @@ def comment_get():
     all_comments = list(db.worthseeing.find({}, {'_id': False}))
     return jsonify({'comments': all_comments})
 
+@app.route('/')
+def home():
+    token_receive = request.cookies.get('mytoken')
+    try:
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+        return render_template('main.html')
+    except jwt.ExpiredSignatureError:
+        return redirect(url_for("login", msg="로그인 시간이 만료되었습니다."))
+    except jwt.exceptions.DecodeError:
+        return redirect(url_for("login", msg="로그인 정보가 존재하지 않습니다."))
+
+
+@app.route('/login')
+def login():
+    msg = request.args.get("msg")
+    return render_template('login.html', msg=msg)
+
+
+@app.route('/sign_in', methods=["POST"])
+def sign_in():
+    # form 타입으로 요청받은 username,password_give를 변수에 저장함
+    username_receive = request.form['username_give']
+    password_receive = request.form['password_give']
+
+    # hash라이브러리의 sha256알고리즘으로 hexdigest함수를 이용하여 pw_hash 변수에 암호화 시킴
+    # db에서 일치하는 아이디와 비밀번호를 찾아 result 변수에 저장
+    pw_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
+    result = db.users.find_one(
+        {'username': username_receive, 'password': pw_hash})
+    print(result)
+    # 결과 값이 있으면 payload에 id와 유효시간을 저장함
+    # token을 SECRET_KEY로 암호화 해줌 / 토큰에는 payload, SECRET_KEY, algorithm이 저장됨
+    if result is not None:
+        payload = {
+            'id': username_receive,
+            'exp': datetime.utcnow() + timedelta(seconds=60 * 60 * 4)  # 로그인 4시간 유지
+        }
+        token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
+
+        return jsonify({'result': 'success', 'token': token})
+
+    # 결과 값을 없으면 결과는 실패, msg를 json형식으로 보내줌
+    else:
+        return jsonify({'result': 'fail', 'msg': '아이디/비밀번호가 일치하지 않습니다.'})
+
+
+# 회원가입 페이지
+@app.route('/sign_up')
+def sign_up_page():
+    return render_template('sign_up.html')
+
+
+@app.route('/sign_up/save', methods=['POST'])
+def sign_up():
+    username_receive = request.form['username_give']
+    password_receive = request.form['password_give']
+    password_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
+
+    doc = {
+        "username": username_receive,  # 아이디
+        "password": password_hash,  # 비밀번호
+        "profile_name": username_receive,  # 프로필 이름 기본값은 아이디
+        # "profile_pic": "",                                          # 프로필 사진 파일 이름
+        # "profile_pic_real": "profile_pics/profile_placeholder.png", # 프로필 사진 기본 이미지
+    }
+
+    db.users.insert_one(doc)
+    return jsonify({'result': 'success'})
+
+
+# 아이디 중복 검사
+@app.route('/sign_up/check_dup', methods=['POST'])
+def check_dup():
+    username_receive = request.form['username_give']
+    exists = bool(db.users.find_one({"username": username_receive}))
+    return jsonify({'result': 'success', 'exists': exists})
+
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
